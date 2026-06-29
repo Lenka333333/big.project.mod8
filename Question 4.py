b5344ba270b932c2d58b2a78f631f1e75bc74fc6
Lenka333333 <lenka.troiakova@gmail.com>
2026-06-29 15:29:41 +0200

    backward solution

--- Question 4.py
+++ Question 4.py
@@ -27,9 +27,7 @@ TRANSITIONS = {
 3: {
 
     # n-state transitions
-    (0, 0, 0, 0): lambda p_ns,p_i,p_e: 0,
     (0,-1, 0, 0): lambda p_ns,p_i,p_e: p_ns*(1-p_i)*(1-p_e),
-    (0, 0, 1, 0): lambda p_ns,p_i,p_e: 0,
     (0,-1, 1, 0): lambda p_ns,p_i,p_e: p_ns*p_i*(1-p_e),
     (0,-1, 0, 1): lambda p_ns,p_i,p_e: p_ns*(1-p_i)*p_e,
     (0,-1, 1, 1): lambda p_ns,p_i,p_e: p_ns*p_i*p_e,
@@ -49,20 +47,20 @@ TRANSITIONS = {
 1: {
 
     # n-state transitions
-    (0, 0, 0, 0): lambda p_ns,p_i,p_e: p_ns*(1-p_i)*(1-p_e),
-    (0, 0, 1, 0): lambda p_ns,p_i,p_e: p_ns*p_i*(1-p_e),
-    (0, 0, 0, 1): lambda p_ns,p_i,p_e: p_ns*(1-p_i)*p_e,
-    (0, 0, 1, 1): lambda p_ns,p_i,p_e: p_ns*p_i*p_e,
+    (0, 0, 0, -1): lambda p_ns,p_i,p_e: p_ns*(1-p_i)*(1-p_e),
+    (0, 0, 1, -1): lambda p_ns,p_i,p_e: p_ns*p_i*(1-p_e),
+    (0, 0, 0, 0): lambda p_ns,p_i,p_e: p_ns*(1-p_i)*p_e,
+    (0, 0, 1, 0): lambda p_ns,p_i,p_e: p_ns*p_i*p_e,
 
     # s-state transitions
-    (1, 0, 0, 0): lambda p_ns,p_i,p_e: 0.16*p_ns*(1-p_i)*(1-p_e),
-    (1, 0, 1, 0): lambda p_ns,p_i,p_e: 0.16*p_ns*p_i*(1-p_e),
-    (1, 1, 0, 0): lambda p_ns,p_i,p_e: 0.84*p_ns*(1-p_i)*(1-p_e),
-    (1, 1, 1, 0): lambda p_ns,p_i,p_e: 0.84*p_ns*p_i*(1-p_e),
-    (1, 0, 0, 1): lambda p_ns,p_i,p_e: 0.16*p_ns*(1-p_i)*p_e,
-    (1, 0, 1, 1): lambda p_ns,p_i,p_e: 0.16*p_ns*p_i*p_e,
-    (1, 1, 0, 1): lambda p_ns,p_i,p_e: 0.84*p_ns*(1-p_i)*p_e,
-    (1, 1, 1, 1): lambda p_ns,p_i,p_e: 0.84*p_ns*p_i*p_e,
+    (1, 0, 0, -1): lambda p_ns,p_i,p_e: 0.16*p_ns*(1-p_i)*(1-p_e),
+    (1, 0, 1, -1): lambda p_ns,p_i,p_e: 0.16*p_ns*p_i*(1-p_e),
+    (1, 1, 0, -1): lambda p_ns,p_i,p_e: 0.84*p_ns*(1-p_i)*(1-p_e),
+    (1, 1, 1, -1): lambda p_ns,p_i,p_e: 0.84*p_ns*p_i*(1-p_e),
+    (1, 0, 0, 0): lambda p_ns,p_i,p_e: 0.16*p_ns*(1-p_i)*p_e,
+    (1, 0, 1, 0): lambda p_ns,p_i,p_e: 0.16*p_ns*p_i*p_e,
+    (1, 1, 0, 0): lambda p_ns,p_i,p_e: 0.84*p_ns*(1-p_i)*p_e,
+    (1, 1, 1, 0): lambda p_ns,p_i,p_e: 0.84*p_ns*p_i*p_e,
 },
 4: {
 
@@ -77,7 +75,6 @@ TRANSITIONS = {
     (1, 0, 0, 0): lambda p_ns, p_i, p_e: 0.16 * p_ns * (1 - p_i) * (1 - p_e),
     (1, 1, 1, 0): lambda p_ns, p_i, p_e: 0.84 * p_ns * p_i * (1 - p_e),
     (1, 1, 0, 0): lambda p_ns, p_i, p_e: 0.84 * p_ns * (1 - p_i) * (1 - p_e),
-
     (1, 0, 1, 1): lambda p_ns, p_i, p_e: 0.16 * p_ns * p_i * p_e,
     (1, 0, 0, 1): lambda p_ns, p_i, p_e: 0.16 * p_ns * (1 - p_i) * p_e,
     (1, 1, 1, 1): lambda p_ns, p_i, p_e: 0.84 * p_ns * p_i * p_e,
@@ -116,9 +113,9 @@ def lambda_It(stage):
 
 
 def p_I(stage):
-    return math.e**(-lambda_It(stage)*time(stage))*lambda_It(stage)*time(stage)
+    return 1 - math.e**(-lambda_It(stage)*time(stage))
 def p_E(stage):
-    return math.e**(-lambda_E*time(stage))*lambda_E*time(stage)
+    return 1 - math.e**(-lambda_E*time(stage))
 def p_NS(stage):
     if schedule[stage-1] == 0:
         return 0
@@ -176,7 +173,13 @@ def transition_prob(state,decision,stage,nextstate):
         return 0
     p_i = p_I(stage)
     p_e = p_E(stage)
-    p_ns = p_NS(stage)
+    if stage >= 32:
+        p_ns = 0
+    else:
+        if ns(nextstate) == schedule[stage]:
+            p_ns = 1
+        else:
+            p_ns = 0
 
     return TRANSITIONS[decision][delta](
         p_ns,
@@ -184,29 +187,115 @@ def transition_prob(state,decision,stage,nextstate):
         p_e,
     )
 
-def nextposiiblestates(state):
+def nextpossiblestates(state,decision,stage):
     nextstates = []
-    list = [0,1,2,3]
-    z = [0,0,0,0]
-    for L in range(len(list) + 1):
-        for subset in itertools.combinations(list, L):
-            zc = z.copy()
-            for i in range(len(subset)):
-                zc[subset[i]] += 1
-            nextstates.append([a + b for a, b in zip(state, zc)])
-    for L in range(len(list) + 1):
-        for subset in itertools.combinations(list, L):
-            zc = z.copy()
-            if len(subset) == 0:
-                continue
-            for i in range(len(subset)):
-                zc[subset[i]] -= 1
 
-            new_state = [a + b for a, b in zip(state, zc)]
-            if all(x >= 0 for x in new_state):
-                nextstates.append(new_state)
-    return nextstates
+    if decision == d_e:
+        if stage == 32 or schedule[stage] == 0:
+            possible_changes = ((0, 0, 0, -1),(0, 0, 1, -1),(0, 0, 0, 0),(0, 0, 1, 0))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
+        if schedule[stage] == 1: #ns intentionaly changed to 0, so I dont add that
+            possible_changes = ((0, 0, 0, -1),(0, 0, 1, -1),(0, 1, 0, -1),(0, 1, 1, -1),(0, 0, 0, 0),(0, 0, 1, 0),(0, 1, 0, 0),(0, 1, 1, 0))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
+    if decision == d_i:
+        if stage == 32 or schedule[stage] == 0:
+            possible_changes = ((0, 0, 0, 0),(0, 0,-1, 0),(0, 0, 0, 1),(0, 0,-1, 1))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
+        if schedule[stage] == 1:
+            possible_changes = ((0, 0, 0, 0),(0, 0,-1, 0),(0, 1, 0, 0),(0, 1,-1, 0),(0, 0, 0, 1),(0, 0,-1, 1),(0, 1, 0, 1),(0, 1,-1, 1))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
+    if decision == d_o:
+        if stage == 32 or schedule[stage] == 0:
+            possible_changes = ((0,-1, 0, 0),(0,-1, 1, 0),(0,-1, 0, 1),(0,-1, 1, 1))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
+        if schedule[stage] == 1:
+            possible_changes = ((0, 0, 0, 0),(0, 0, 1, 0),(0,-1, 0, 0),(0,-1, 1, 0),(0, 0, 0, 1),(0, 0, 1, 1),(0,-1, 0, 1),(0,-1, 1, 1))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
+    if decision == d_n:
+        if stage == 32 or schedule[stage] == 0:
+            possible_changes = ((0, 0, 1, 0),(0, 0, 0, 0),(0, 0, 1, 1),(0, 0, 0, 1))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
+        if schedule[stage] == 1:
+            possible_changes = ((0, 0, 1, 0),(0, 0, 0, 0),(0, 1, 1, 0),(0, 1, 0, 0),(0, 0, 1, 1),(0, 0, 0, 1),(0, 1, 1, 1),(0, 1, 0, 1))
+            for i in possible_changes:
+                nextstates.append([a + b for a, b in zip(i, state)])
+            return nextstates
 
+
+V = {}
+reachable = {}
+decisions = (d_n,d_o,d_i,d_e)
+def backwardsolution():
+    reachable[1] = {(0, 0, 0, 0)}
+    for stage in range(1, 33):
+        reachable[stage + 1] = set()
+
+        for state in reachable[stage]:
+            for decision in decisions:
+                for next_state in nextpossiblestates(state, decision, stage):
+                    reachable[stage + 1].add(next_state)
+    '''
+    stage = 33
+    ns = 0
+    for o in range(stage):
+        for i in range(stage):
+            for e in range(stage):
+                state = (ns, o, i, e)
+                V[(33,state)] = revenue(state,0,33)
+    for stagei in range(32,0,-1):
+        ns = schedule[stagei-1]
+        for o in range(stagei):
+            for i in range(stagei):
+                for e in range(stagei):
+    '''
+                    state = (ns, o, i, e)
+                    bestvalue = -float("inf")
+                    bestdecision = None
+
+                    decisions = []
+                    if i > 0 and e == 0:
+                        decisions.append(d_i)
+
+                    if e > 0:
+                        decisions.append(d_e)
+
+                    if o > 0 and e == 0:
+                        decisions.append(d_o)
+
+                    if o == 0 and i == 0 and e == 0:
+                        decisions.append(d_n)
+
+                    for decision in decisions:
+                        val = revenue(state,decision, stagei)
+
+                        for nextst in nextposiiblestates(state, decision, stagei):
+                            val += (transition_prob(state,decision,stagei,nextst)*V[(stagei+1,tuple(nextst))])
+
+                        if val > bestvalue:
+                            bestvalue = val
+                            bestdecision = decision
+
+                    V[(stagei,state)] = bestvalue
+                    policy[(stagei,state)] = bestdecision
+    return V,policy
+
+
+
+'''
 def recurrence_relation(stage, state):
     if stage == 33:
         return revenue(state,0, 33), None #independent of the decision
@@ -231,26 +320,15 @@ def recurrence_relation(stage, state):
         return best_value, best_decision
 
 
-def sumrec(state,decision, stage):
-    sums = 0
-    if decision == d_i:
-        for st in nextposiiblestates(state):
-            sums += transition_prob(state, d_i, stage, st) * recurrence_relation(stage + 1, st)[0]
-        return sums
-    if decision == d_e:
-        for st in nextposiiblestates(state):
-            sums += transition_prob(state, d_e, stage, st) * recurrence_relation(stage + 1, st)[0]
-        return sums
-    if decision == d_o:
-        for st in nextposiiblestates(state):
-            sums += transition_prob(state, d_o, stage, st) * recurrence_relation(stage + 1, st)[0]
-        return sums
-    if decision == d_n:
-        for st in nextposiiblestates(state):
-            sums += transition_prob(state, d_n, stage, st) * recurrence_relation(stage + 1, st)[0]
-        return sums
+def sumrec(state, decision, stage):
+    total = 0
+    for st in nextpossiblestates(state, decision, stage):
+        total += (
+            transition_prob(state, decision, stage, st)
+            * recurrence_relation(stage+1, tuple(st))[0]
+        )
+    return total
+'''
+print(backwardsolution())
 
-
-print(recurrence_relation(32,(0,2,2,0)))
-print(policy)
 #print(nextposiiblestates((2,0,0,0)))
